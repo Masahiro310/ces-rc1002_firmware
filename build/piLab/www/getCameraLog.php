c9dc26f7923c0b556a2a5f405db2174ded2536e3
Masahiro <orishow.llc@gmaile.com>
2026-09-14 13:20:16 +0900

    ログの表示方法を変更

--- build/piLab/www/getCameraLog.php
+++ build/piLab/www/getCameraLog.php
@@ -6,13 +6,13 @@ $xmlData = simplexml_load_file($xml); //xmlを読み込む
 <table border="2" width="750px">
 	<tr>
 		<th width="200">日時</th>
-		<th width="150">カメラ/回転台</th>
+		<th width="150">カメラID</th>
 		<th>ログ内容</th>
 	</tr>
 
 	<?php
 	foreach ($xmlData as $log) {
-		print("<tr><td>$log->Date</td><td>" . str_pad($log->Unit, 2, 0, STR_PAD_LEFT) ." CH</td><td>$log->Message</td></tr>\n");
+		print("<tr><td>$log->Date</td><td>" . str_pad($log->Unit, 2, 0, STR_PAD_LEFT) ."</td><td>$log->Message</td></tr>\n");
 	}
 	?>
 </table>
